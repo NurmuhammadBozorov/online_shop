bff0ce18ff876d2d6deef2a181ff7061f30f891f
Javlon2 <choriyevjavlon5@gmail.com>
2024-04-02 22:24:05 +0500

    Salom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
+// import Navbar from './components/Javlon/Navbar/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import Block from './components/Javlon/Block/index'
+import Footer from './components/Javlon/Footer/footer';
 
 const App = () => {
   return (
-    <div>App</div>
+    // <Block/>
+    //  <Navbar/>
+    <Footer/>
   )
 }
 
---
+++ src/components/Javlon/Footer/footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './footer.css'
+
+const footer = () => {
+  return (
+    <div>
+      <div className='qism'>
+          <div >
+            
+          </div>  
+      </div>
+    </div>
+  )
+}
+
+export default footer
